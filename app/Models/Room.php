70ecd189e56fc2a8411f3783b172763f743f3111
Sendiko <sarangtawon897@gmail.com>
2024-05-29 17:51:57 +0700

    add fillable and HasMany mehod

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'number'
+    ];
+
+    function guests() : HasMany {
+        return $this->hasMany(Guest::class);
+    }
 }
