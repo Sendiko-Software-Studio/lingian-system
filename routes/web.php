0b059a8c1c8e5523557451de1401a72a72194a89
Sendiko <sarangtawon897@gmail.com>
2024-05-29 18:23:31 +0700

    add routes for public ui

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/room', [PublicController::class, 'index'])->name('room.index');
+Route::get('/room/{id}', [PublicController::class, 'show'])->name('room.show');
+Route::get('/room/{id}/edit', [PublicController::class, 'edit'])->name('room.edit'); 
+Route::post('/room/{id}/update', [PublicController::class, 'update'])->name('room.update');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
