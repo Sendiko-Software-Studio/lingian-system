b7d2a1f570bdaaefc6ac983be38dc8315bf9577a
Sendiko <sarangtawon897@gmail.com>
2024-05-30 07:34:39 +0700

    save status(forgor)

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -37,11 +37,13 @@ class GuestController extends Controller
     {
         Guest::create([
             'room_id' => $request->room_number,
-            'nama_tamu' => $request->guest_name1
+            'nama_tamu' => $request->guest_name1,
+            'status' => $request->status1
         ]);
         Guest::create([
             'room_id' => $request->room_number,
-            'nama_tamu' => $request->guest_name2
+            'nama_tamu' => $request->guest_name2,
+            'status' => $request->status2
         ]);
         return redirect()->route('admin.guests.index');
     }
